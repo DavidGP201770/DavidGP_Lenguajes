c79cc5d1b60b8e045e76982519796fac68542b5d
DUGPLPW7 <DUGPLPW7@10.20.1.225>
2017-12-12 02:27:53 -0600

    27 de Noviembre :v THREADS FINAL

--- src/ejercicioespecial2/PersistenciaCliente.java
+++ src/ejercicioespecial2/PersistenciaCliente.java
@@ -91,7 +91,8 @@ public class PersistenciaCliente {
           
           for(Cliente c:registros)
           {
-              System.out.println("id "+c.getId()+" Nombre "+c.getNombre());
+              System.out.println("id \n"+c.getId()+" Nombre"+c.getNombre());
+              System.out.println("\n Edad: "+c.getEdad()+" \n Direccion: "+c.getDireccion());
           }
             
             stc.close();
@@ -112,15 +113,129 @@ public class PersistenciaCliente {
         return mensaje;    
     }
     
-    public String actualizarCliente(){
+    public String actualizarCliente(Cliente c, Direccion d,int id){
+        Connection con=null;
+          String mensaje="";
+         // conexion;
+        try{
+            con= conexion.Conexion.conectarse("root", "root");
+            System.out.println("Te conectaste!!");
+            
+             PreparedStatement std = con.prepareStatement("update Direccion set id=?, cp=?, calle=?, municipio=? where id="+id);
+               std.setInt(1, d.getId());
+               std.setInt(2, (int) d.getCp());
+               std.setString(3,d.getCalle() );
+               std.setString(4, d.getMunicipio());
+               std.execute();
+            
+            PreparedStatement stc = con.prepareStatement("update Cliente set id=?, nombre=?, edad=?, direccion=? where id="+id);
+            
+            stc.setInt(1, 1);
+            stc.setString(2,c.getNombre());
+            stc.setInt(3, c.getEdad());
+            stc.setInt(4, d.getId());
+            //invocar o dar enter 
+            stc.execute();
+            
+            mensaje="Registro actualizado";
+            std.close();
+            stc.close();
+          
+        }
+        catch(ClassNotFoundException e){
+            mensaje="No se cargo bien el driver";
+        }catch(SQLException e){
+            mensaje=e.getMessage();
+        }finally{
+            try{
+                if(con!=null)con.close();
+                
+            }catch(SQLException ex){
+                mensaje="No se cargo bien en sql";
+            }
+        } 
            return mensaje; 
     }
     
     public String buscarTodoslosCliente(){
+        Connection con=null;
+          String mensaje="";
+         // conexion;
+        try{
+            con= conexion.Conexion.conectarse("root", "root");
+            System.out.println("Te conectaste!!");
+            
+            Statement stc=con.createStatement();
+            ResultSet rs= stc.executeQuery("select * from cliente");
+            
+            ArrayList<Cliente> registros=new ArrayList<>();
+          while(rs.next())
+          {    
+              Cliente c= new Cliente();
+              c.setId(rs.getInt(1));
+              c.setNombre(rs.getString(2));
+              c.setEdad(rs.getInt(3));
+              int idDireccion=  rs.getInt(4);
+              Direccion d=new Direccion(idDireccion);
+              c.setDireccion(d);
+              registros.add(c);
+          }
+          
+          for(Cliente c:registros)
+          {
+              System.out.println("id: "+c.getId()+"\n Nombre "+c.getNombre());
+              System.out.println("\n Edad: "+c.getEdad()+" \n Direccion: "+c.getDireccion().getId());
+          }
+            
+            stc.close();
+          
+        }
+        catch(ClassNotFoundException e){
+            mensaje="No se cargo bien el driver";
+        }catch(SQLException e){
+            mensaje=e.getMessage();
+        }finally{
+            try{
+                if(con!=null)con.close();
+                
+            }catch(SQLException ex){
+                mensaje="No se cargo bien en sql";
+            }
+        }
           return mensaje;  
     }
     
-    public String borrarCliente(){
+    public String borrarCliente(int id){
+          Connection con=null;
+          String mensaje="";
+         // conexion;
+        try{
+            con= conexion.Conexion.conectarse("root", "root");
+            System.out.println("Te conectaste!!");
+            
+            PreparedStatement std = con.prepareStatement("delete from Direccion where id="+id);   
+            std.execute();
+            
+            PreparedStatement stc = con.prepareStatement("delete from Cliente where id="+id);
+            stc.execute();
+         
+            mensaje="Registro borrado";
+            std.close();
+            stc.close();
+          
+        }
+        catch(ClassNotFoundException e){
+            mensaje="No se cargo bien el driver";
+        }catch(SQLException e){
+            mensaje=e.getMessage();
+        }finally{
+            try{
+                if(con!=null)con.close();
+                
+            }catch(SQLException ex){
+                mensaje="No se cargo bien en sql";
+            }
+        }
           return mensaje;  
     }
 }
--- src/ejercicioespecial2/ProbarCliente.java
+++ src/ejercicioespecial2/ProbarCliente.java
@@ -13,9 +13,20 @@ public class ProbarCliente {
     public static void main(String[] args) {
         Direccion d=new Direccion(1,55130l, "mexico", "Ecatepec");
         Cliente c=new Cliente(1, "David", 20,d);
+        
         PersistenciaCliente pc=new PersistenciaCliente();
+        
         try{
-            System.out.println(pc.guardarCliente(c, d));
+            //GUARDAR CLIENTE
+            //System.out.println(pc.guardarCliente(c, d));
+            
+            //BUSCAR TODOS
+            //System.out.println(pc.buscarTodoslosCliente());
+            
+            //BORRAR CLIENTE
+            //System.out.println(pc.borrarCliente(1));
+            
+            
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
